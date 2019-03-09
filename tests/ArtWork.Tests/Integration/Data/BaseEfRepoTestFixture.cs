a32ba72e5c7c9a3f231fff5fdc552a609852d260
incubus8 <rheza.satria.ta@gmail.com>
2019-03-10 00:27:30 +0700

    refactor from base template

--- tests/ArtWork.Tests/Integration/Data/BaseEfRepoTestFixture.cs
+++ tests/ArtWork.Tests/Integration/Data/BaseEfRepoTestFixture.cs
@@ -21,7 +21,7 @@ namespace ArtWork.Tests.Integration.Data
             // Create a new options instance telling the context to use an
             // InMemory database and the new service provider.
             var builder = new DbContextOptionsBuilder<AppDbContext>();
-            builder.UseInMemoryDatabase("cleanarchitecture")
+            builder.UseInMemoryDatabase("artwork")
                    .UseInternalServiceProvider(serviceProvider);
 
             return builder.Options;
